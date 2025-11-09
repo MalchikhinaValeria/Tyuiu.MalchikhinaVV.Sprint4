5c308bde3458b0822da775d8ca9d14e5789f141f
Home <Home@LAPTOP-L43VJUUA>
2025-11-09 21:13:29 +0500

    Task5

---
+++ Tyuiu.MalchikhinaVV.Sprint4.Task5.V9.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MalchikhinaVV.Sprint4.Task5.V9.Lib;
+namespace Tyuiu.MalchikhinaVV.Sprint4.Task5.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix =
+            {
+                { 1,  0,  2,  -2, -1 },
+                { -4, -1,  3,  4,  0 },
+                { -2,  2, 3,  -4,  1 },
+                { 4, -3,  1,  1,  -1 },
+                { 3,  -3, -1,  2, -4 }
+            };
+
+            int wait = 12;
+            int result = ds.Calculate(matrix);
+
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.MalchikhinaVV.Sprint4.Task5.V9/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MalchikhinaVV.Sprint4.Task5.V9.Lib;
+namespace Tyuiu.MalchikhinaVV.Sprint4.Task5.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #4 | Выполнил: Мальчихина В. В. | ИИПб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы (генератор случайных чисел)                     *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнил: Мальчихина Валерия Викторовна | ИИПб-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов,                    *");
+            Console.WriteLine("* заполненный  случайными значениями в диапазоне от -4 до 4.              *");
+            Console.WriteLine("* Найти количество положительных элементов.                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Исходный массив: ");
+
+            Random rnd = new Random();
+            int[,] matrix = new int[5, 5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = rnd.Next(-4, 5);
+                    Console.Write($"{matrix[i, j],4}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Количество положительных элементов: " + ds.Calculate(matrix));
+            Console.ReadKey();
+
+        }
+    }
+}
+
